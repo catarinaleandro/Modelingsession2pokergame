53f963357733b1c430dd146467550c51ed0c02a7
catarinaleandro <catatorresleandro@gmail.com>
2025-05-30 18:19:57 +0100

    Added docstrings

--- deck.py
+++ deck.py
@@ -1,9 +1,14 @@
 import random
 class Card:
-    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J","Q", "K", "A"]
     SUITS = ["♣", "♦", "♥", "♠"]
-    # SUITS = ["clubs", "diamonds", "hearts", "spades"]
+    #SUITS = clubs, diamonds, hearts and spades
     def __init__(self, rank, suit):
+        """
+        Creates a card by validating the rank and suit are in their respective lists
+        :param rank: rank of the card
+        :param suit:suit of the card
+        """
         if rank not in self.RANKS:
             raise ValueError("invalid rank")
         if suit not in self.SUITS:
@@ -13,26 +18,55 @@ class Card:
 
     @property
     def rank(self):
+        """
+        Decorator that allows you to call the rank of a card as .rank instead of ._rank
+        :return: rank
+        """
         return self._rank
 
     @property
     def suit(self):
+        """
+        Decorator that allows you to call the suit of a card as .suit instead of ._suit
+        :return: suit
+        """
         return self._suit
 
     def __str__(self):
+        """
+        Magic method that defines the card as a string
+        :return: string showing the rank and suit of a card
+        """
         return f"{self._rank}{self._suit}"
 
     def __repr__(self):
-        return self.__str__() # repr is the same as str
+        """
+        Magic method that prints a string representation of the list of cards held within a container
+        :return: list of cards
+        """
+        return self.__str__() #repr is the same as str
 
     def __eq__(self, other):
+        """
+        Checks if two cards are equal based on their rank
+        :param other: second card
+        :return: True if equal, False if different
+        """
         return self.rank == other.rank
 
     def __lt__(self, other):
+        """
+        Checks if a card is less than another by looking at their rank
+        :param other: second card
+        :return: True if less, False if bigger
+        """
         return self.RANKS.index(self.rank) < self.RANKS.index(other.rank)
 
 class Deck:
     def __init__(self):
+        """
+        Magic method that creates a standard deck of cards by looping through all combinations of rank and suits and appending each new object to a list
+        """
         _cards = []
         for suit in Card.SUITS:
             for rank in Card.RANKS:
@@ -41,19 +75,35 @@ class Deck:
 
     @property
     def cards(self):
+        """
+        Magic method that allows you to call the deck as .cards instead of ._cards
+        :return: deck of cards
+        """
         return self._cards
 
     def __str__(self):
+        """
+        Magic method that prints the deck as a string
+        :return: deck of cards as a string
+        """
         return str(self._cards)
 
     def shuffle(self):
+        """
+        Method to shuffle the deck of cards
+        :return: shuffled deck
+        """
         random.shuffle(self.cards)
 
     def deal(self):
+        """
+        Method to modify the deck by removing the first card from the list and returning  the top card from the deck
+        :return: top card from the deck
+        """
         return self.cards.pop(0)
 
-if __name__ == "__main__":
-    c1 = Card("A", "♣")
+if __name__=="__main__":
+    c1 = Card("A","♣")
     print(c1.suit, c1.rank)
     deck = Deck()
     print(deck)
--- game.py
+++ game.py
@@ -2,6 +2,10 @@ from deck import Deck, Card
 
 class Hand:
     def __init__(self, deck):
+        """
+        Method that deals a five card hand
+        :param deck: poker deck
+        """
         cards = []
         for i in range(5):
             cards.append(deck.deal())
@@ -9,20 +13,36 @@ class Hand:
 
     @property
     def cards(self):
+        """
+        Decorator that allows you to call the deck as .cards instead of ._cards
+        :return: deck of cards
+        """
         return self._cards
 
     def __str__(self):
+        """
+        Magic method that prints the deck as a string
+        :return: deck of cards as a string
+        """
         return str(self.cards)
 
     @property
     def is_flush(self):
-        for card in self.cards[1:]:
+        """
+        Method that checks if the hand is a flush or not
+        :return: True if flush, False if otherwise
+        """
+        for card in self._cards[1:]:
             if self.cards[0].suit != card.suit:
                 return False
         return True
 
     @property
     def num_matches(self):
+        """
+        Goes over each hand and returns the amount of matches
+        :return: total number of matches
+        """
         matches = 0
         for i in range(5):
             for j in range(5):
@@ -34,30 +54,50 @@ class Hand:
 
     @property
     def is_pair(self):
+        """
+        Method that checks if the hand has a pair
+        :return: True if there is one pair, False if there is no pair
+        """
         if self.num_matches == 2:
             return True
         return False
 
     @property
     def is_2_pair(self):
+        """
+        Method that checks if the hand has two pairs
+        :return: True if there is two pairs, False if otherwise
+        """
         if self.num_matches == 4:
             return True
         return False
 
     @property
     def is_trips(self):
+        """
+        Method that checks if the hand has three pairs
+        :return: True if there is three pairs, False if otherwise
+        """
         if self.num_matches == 6:
             return True
         return False
 
     @property
     def is_quads(self):
+        """
+        Method that checks if the hand has six pairs
+        :return: True if there is six pairs, False if otherwise
+        """
         if self.num_matches == 12:
             return True
         return False
 
     @property
     def is_full_house(self):
+        """
+        Checks if hand is a full house, 8 matches
+        :return: True if full house, False if otherwise
+        """
         if self.num_matches == 8:
             return True
         else:
@@ -65,13 +105,19 @@ class Hand:
 
     @property
     def is_straight(self):
+        """
+        Checks if a hand is straight by validating if there no matches and the difference between the highest and lowest card is 4.
+        ...
+        :return: True if straight, False if otherwise
+        """
         if self.num_matches != 0:
             return False
         self.cards.sort()
-        if Card.RANKS.index(self.cards[-1].rank) != \
-                Card.RANKS.index(self.cards[0].rank) + 4:
+        if Card.RANKS.index(self.cards[-1].rank) != Card.RANKS.index(self.cards[0].rank) + 4:
             return False
         return True
+
+
 matches = 0
 count = 0
 while matches < 1000:
@@ -80,9 +126,9 @@ while matches < 1000:
     hand = Hand(deck)
     count += 1
     if hand.is_straight:
-        # print(hand)
+        #print(hand)
         matches += 1
-        # break
+       # break
 
 print(f"The probability of straight is {100*matches/count}%")
 
